d6b9c43867d13f6f2782d2cb36bc32d4b026b937
Ahmed Bahgat <ahmedbahgatnabih@gmail.com>
2020-10-15 18:56:33 +0200

    Fixing message repetetion

--- ChatApp/Modules/Chat/ChatViewController.swift
+++ ChatApp/Modules/Chat/ChatViewController.swift
@@ -70,21 +70,6 @@ final class ChatViewController: MessagesViewController {
     }
     
     //MARK:-Functions
-    @objc func loadMoreMessages() {
-        limit += 5
-        reference = db.collection(["channels", channel.id ?? "", "thread"].joined(separator: "/")).limit(toLast: limit)
-        
-        messageListener = reference?.addSnapshotListener { querySnapshot, error in
-            guard let snapshot = querySnapshot else {
-                print("Error listening for channel updates: \(error?.localizedDescription ?? "No error")")
-                return
-            }
-            
-            snapshot.documentChanges.forEach { [weak self] change in
-                self?.addNewMessages(change)
-            }
-        }
-    }
     private func insertNewMessage(_ message: Message) {
         guard !messages.contains(message) else {
             return
@@ -117,7 +102,11 @@ final class ChatViewController: MessagesViewController {
     }
     private func addNewMessages(_ change:DocumentChange) {
         let message = Message(document: change.document)!
+        guard !messages.contains(message) else {
+            return
+        }
         self.messages.insert(message, at: 0)
+        messages.sort()
         self.messagesCollectionView.reloadDataAndKeepOffset()
         self.refreshControl.endRefreshing()
     }
@@ -129,6 +118,22 @@ final class ChatViewController: MessagesViewController {
             }
             
             self.messagesCollectionView.scrollToBottom()
+        }
+    }
+    
+    @objc func loadMoreMessages() {
+        limit += 5
+        reference = db.collection(["channels", channel.id ?? "", "thread"].joined(separator: "/")).limit(toLast: limit)
+        
+        messageListener = reference?.addSnapshotListener { querySnapshot, error in
+            guard let snapshot = querySnapshot else {
+                print("Error listening for channel updates: \(error?.localizedDescription ?? "No error")")
+                return
+            }
+            
+            snapshot.documentChanges.forEach { [weak self] change in
+                self?.addNewMessages(change)
+            }
         }
     }
     deinit {
